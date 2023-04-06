f23349685e1264c99b0109bf0d93445f64fcaa94
xuanbin <1779552618@qq.com>
2023-04-06 22:36:45 +0800

    second hot-fix commit

--- src/main/java/com/xuebi/GitTest.java
+++ src/main/java/com/xuebi/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello git3");
         System.out.println("这是hot-fix分支");
+        System.out.println("1111111");
     }
 }
